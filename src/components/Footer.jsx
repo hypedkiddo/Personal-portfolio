5e160a89033e355c938f98757cc9db9954eddce9
crazypunk08 <naren.lakamannavar@gmail.com>
2025-07-15 09:49:43 +0530

    Edited footer.jsx

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,11 @@
-import { ArrowUp } from "lucide-react";
+import { ArrowUp, HeartIcon } from "lucide-react";
 
 export const Footer = () => {
   return (
     <footer className="py-12 px-4 bg-card relative border-t border-border mt-12 pt-8 flex flex-wrap justify-between items-center">
-      {" "}
-      <p className=" flex items-center justify-center text-sm text-muted-foreground">
-        {" "}
-        &copy; {new Date().getFullYear()}  All rights reserved.
+      <p className="w-full flex items-center justify-center text-sm text-muted-foreground text-center">
+        Developed with{" "}
+        <HeartIcon color="red" fill="red" className="mx-1" /> by Naren
       </p>
       <a
         href="#hero"
